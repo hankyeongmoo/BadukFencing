8e91c67c54f88a52d0e2c372ee4072cbd78f73ac
hankyeongmoo <165135691+hankyeongmoo@users.noreply.github.com>
2026-02-04 16:01:54 +0900

    스테미나 (게이지) 구현

--- Assets/Scripts/Game/Manager/Manager_Move.cs
+++ Assets/Scripts/Game/Manager/Manager_Move.cs
@@ -3,11 +3,4 @@ using UnityEngine;
 public class Manager_Move : MonoBehaviour
 {
     static public int[] movePoint = new int[2] { 0, 0 };
-
-    // public void PlayerPosVisualization(int playerIndex)
-    // {
-    //     // 이 함수는 플레이어 위치 시각화를 담당합니다.
-    //     // 실제 구현은 PlayerControl 스크립트에서 처리됩니다.
-
-    // }
 }
--- Assets/Scripts/Game/Player/PlayerControl.cs
+++ Assets/Scripts/Game/Player/PlayerControl.cs
@@ -1,8 +1,16 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerControl : MonoBehaviour
 {
     public int playerIndex = 0; // 플레이어 인덱스 (0 또는 1)
+
+    [Header("스태미너")]
+    public float currentStamina = 200f;
+    public float maxStamina = 200f;
+    public float staminaRegenRate = 200f; // 초당 회복량
+    public Slider staminaSlider1;
+    public Slider staminaSlider2;
 
     void Start()
     {
@@ -11,23 +19,45 @@ public class PlayerControl : MonoBehaviour
 
     void Update()
     {
-        // 좌우 화살표 키 입력 처리
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        // 이동 (좌우 화살표 키 입력 처리)
+        if (currentStamina > 100f)
         {
-            if (Manager_Move.movePoint[playerIndex] > 1)
+            if (Input.GetKeyDown(KeyCode.LeftArrow) && Manager_Move.movePoint[playerIndex] > 1)
             {
                 Manager_Move.movePoint[playerIndex] -= 1;
+                currentStamina -= 100f;
             }
-        }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            if (Manager_Move.movePoint[playerIndex] < 11)
+            else if (Input.GetKeyDown(KeyCode.RightArrow) && Manager_Move.movePoint[playerIndex] < 11)
             {
                 Manager_Move.movePoint[playerIndex] += 1;
+                currentStamina -= 100f;
             }
         }
 
         // 현재 위치 시각화
         transform.position = new Vector3((Manager_Move.movePoint[playerIndex] - 6) * 1.5f, 0, 0);
+        
+        // 스태미너 UI 업데이트
+        if (currentStamina < 100f)
+        {
+            staminaSlider1.value = currentStamina / 100f;
+            staminaSlider2.value = 0f;
+        }
+        else if (currentStamina >= 100f)
+        {
+            staminaSlider1.value = 1f;
+            staminaSlider2.value = (currentStamina - 100) / 100f;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // 스태미너 회복
+        if (currentStamina < maxStamina)
+        {
+            currentStamina += staminaRegenRate * Time.fixedDeltaTime;
+            if (currentStamina > maxStamina)
+                currentStamina = maxStamina;
+        }
     }
 }
