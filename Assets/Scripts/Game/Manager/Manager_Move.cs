48786c4c3ee95499b74b8b8407dba2c65dec3f35
hankyeongmoo <165135691+hankyeongmoo@users.noreply.github.com>
2026-02-04 14:59:40 +0900

    플레이어 이동 (array 사용)

--- Assets/Scripts/Game/Manager/Manager_Move.cs
+++ Assets/Scripts/Game/Manager/Manager_Move.cs
@@ -2,15 +2,12 @@ using UnityEngine;
 
 public class Manager_Move : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    static public int[] movePoint = new int[2] { 0, 0 };
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // public void PlayerPosVisualization(int playerIndex)
+    // {
+    //     // 이 함수는 플레이어 위치 시각화를 담당합니다.
+    //     // 실제 구현은 PlayerControl 스크립트에서 처리됩니다.
+
+    // }
 }
--- Assets/Scripts/Game/Player/PlayerControl.cs
+++ Assets/Scripts/Game/Player/PlayerControl.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int playerIndex = 0; // 플레이어 인덱스 (0 또는 1)
+
     void Start()
     {
-        
+        Manager_Move.movePoint[playerIndex] = 3; // 세 번째 칸에 존재
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // 좌우 화살표 키 입력 처리
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (Manager_Move.movePoint[playerIndex] > 1)
+            {
+                Manager_Move.movePoint[playerIndex] -= 1;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (Manager_Move.movePoint[playerIndex] < 11)
+            {
+                Manager_Move.movePoint[playerIndex] += 1;
+            }
+        }
+
+        // 현재 위치 시각화
+        transform.position = new Vector3((Manager_Move.movePoint[playerIndex] - 6) * 1.5f, 0, 0);
     }
 }
